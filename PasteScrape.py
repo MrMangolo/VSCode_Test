8a41e2f56249b549d5f7e9934572c04ac23e210c
Daniel Weißbacher <weissbacherd@ai.pri>
2019-04-05 15:23:52 +0200

    Added Basics

--- PasteScrape.py
+++ PasteScrape.py
@@ -1,6 +1,34 @@
 import argparse
+import requests
+import re
+import sys
+import json
 from src.argparser import AllArgs
 
 #
 arguments = AllArgs()
 arguments.parseArgs()
+
+class PasteLists:
+    latest_Entry = dict()
+    new_List = None
+
+def testAccess():
+    print("Testing whitelisted ip...")
+    r = requests.get("https://scrape.pastebin.com/api_scraping.php")
+    z = re.match("YOUR IP: \d+.\d+.\d+.\d+ DOES NOT HAVE ACCESS.", r.text)
+    if(z):
+        sys.exit("Please whitelist your Ip")
+    else:
+        print("Whitelist: OK")
+
+testAccess()
+
+def getPasteList():
+    r = requests.get("https://scrape.pastebin.com/api_scraping.php")
+    PasteLists.new_List = json.loads(r.text)
+    if(PasteLists.new_List[-1] not in PasteLists.latest_Entry):
+        print("ITS NOT IN IT")
+    else:
+        PasteLists.latest_Entry = PasteLists.new_List[-1]
+    
--- src/argparser.py
+++ src/argparser.py
@@ -6,10 +6,10 @@ class AllArgs:
 
     #Set Vars to Arguments
     def parseArgs(self):
-        parser = argparse.ArgumentParser(description="This is a Pastebin Scraper. It scrapes all new Pastes at the time it's getting started.")
+        parser = argparse.ArgumentParser(description="This is a Pastebin Scraper. It scrapes all new Pastes at the time it's getting started. Go to https://pastebin.com/api_scraping_faq and Whitelist you IP to use the Scraper.")
         parser._action_groups.pop()
-        required = parser.add_argument_group("Required arguments")
-        optional = parser.add_argument_group("Optional arguments")
-        required.add_argument("-k", "--key", help="Input pastebin key", required=True)
-        args = parser.parse_args()
-        self.key = args.key
+        #required = parser.add_argument_group("Required arguments")
+        #optional = parser.add_argument_group("Optional arguments")
+        #required.add_argument("-k", "--key", help="Input pastebin key", required=True)
+        #args = parser.parse_args()
+        #self.key = args.key
